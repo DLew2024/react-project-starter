0e2a5828361371efa0d570d22db37dd89b34fa65
DLew <96327749+DLew2024@users.noreply.github.com>
2025-01-05 21:53:07 -0600

    Updated function to fetch data

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,20 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const fetchUsers = createAsyncThunk("users/fetch", async () => {
+export const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
   // DEV ONLY
-  await pause(10000);
+  await pause(1000);
 
   return response.data;
 });
 
-// DEV ONLY
+// DEV ONLY - add 1 second pause
 const pause = (duration) => {
   return new Promise((resolve) => {
     setTimeout(resolve, duration);
   });
 };
-
-export { fetchUsers };
