0ebccbb9a7f66bec9376bf0d2bd7b1647285ccc7
DLew <96327749+DLew2024@users.noreply.github.com>
2025-01-08 23:02:42 -0600

    Deletes a user along with all of his content

--- src/store/thunks/deleteUser.js
+++ src/store/thunks/deleteUser.js
@@ -2,6 +2,21 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const deleteUser = createAsyncThunk("users/delete", async (user) => {
+  const { data: userAlbums } = await axios.get(
+    `http://localhost:3005/albums?userId=${user.id}`
+  );
+
+  for (let album of userAlbums) {
+    const { data: albumPhotos } = await axios.get(
+      `http://localhost:3005/photos?albumId=${album.id}`
+    );
+
+    for (let photo of albumPhotos) {
+      await axios.delete(`http://localhost:3005/photos/${photo.id}`);
+    }
+
+    await axios.delete(`http://localhost:3005/albums/${album.id}`);
+  }
   await axios.delete(`http://localhost:3005/users/${user.id}`);
 
   return user;
